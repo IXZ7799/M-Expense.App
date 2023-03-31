7bc2c1bebd1ca4b291aff9d05fba85b627a4e64a
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-03-31 17:28:09 +0100

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,22 +9,34 @@ public partial class MainPage : ContentPage
 
     private void searchTrip_SearchButtonPressed(object sender, EventArgs e)
     {
+        string keyword = this.searchUser.Text;
 
+        if (keyword == null || keyword.Length == 0)
+        {
+            populateUserData();
+        }
+        else
+        {
+            collectionView.ItemsSource = App.UserDatabase.SearchUserByName(keyword);
+        }
     }
 
     private void collectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-
+        User selectedCust = (e.CurrentSelection.FirstOrDefault() as User);
+        Navigation.PushModalAsync(new UserDetails(selectedCust, true), true);
     }
 
     private void btnAdd_Clicked(object sender, EventArgs e)
     {
-
+        User newCust = new User("", "", 0, UserCollection.DEFAULT_IMAGE);
+        Navigation.PushModalAsync(new UserDetails(newCust, false), true);
     }
 
     private void btnDeleteAll_Clicked(object sender, EventArgs e)
     {
-
+        App.UserDatabase.DeleteAllUsers();
+        populateUserData();
     }
 }
 
