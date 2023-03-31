0db2db2e70707299ed1f68e3824c04806b3dd223
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-04-01 00:23:14 +0100

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace M_Expense;
+
+[Table("User")]
+public class User
+{
+    [MaxLength(250)]
+    public string TripName { get; set; }
+    public string Destination { get; set; }
+    public string TripDate { get; set; }
+    public string RiskAssessment { get; set; }
+    public string TripDesc { get; set; }
+    public int PeopleAttending { get; set; }
+    public string Transportation { get; set; }
+
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public string ImageName { get; set; }
+
+    public User()
+    {
+        ID = 0;
+        ImageName = UserSQLiteDatabase.DEFAULT_IMAGE;
+    }
+
+    public User(string inTripName, string inDestination, string inTripDate, string inRiskAssessment, string inTripDesc, int inPeopleAttending, string inTransportation, int inID)
+    {
+        TripName = inTripName;
+        Destination = inDestination;
+        TripDate = inTripDate;
+        RiskAssessment = inRiskAssessment;
+        TripDesc = inTripDesc;
+        PeopleAttending = inPeopleAttending;
+        Transportation = inTransportation;
+        ID = inID;
+        ImageName = UserSQLiteDatabase.DEFAULT_IMAGE;
+    }
+
+    public User(string inTripName, string inDestination, string inTripDate, string inRiskAssessment, string inTripDesc, int inPeopleAttending, string inTransportation, int inID, string inImageName)
+    {
+        TripName = inTripName;
+        Destination = inDestination;
+        TripDate = inTripDate;
+        RiskAssessment = inRiskAssessment;
+        TripDesc = inTripDesc;
+        PeopleAttending = inPeopleAttending;
+        Transportation = inTransportation;
+        ID = inID;
+        ImageName = inImageName;
+    }
+}
