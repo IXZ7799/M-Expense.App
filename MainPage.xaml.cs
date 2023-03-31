a627469e78465760b1a1fc8bb91d9723662ab071
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-03-31 16:14:40 +0100

    Added buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,5 +20,25 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+    private void searchTrip_SearchButtonPressed(object sender, EventArgs e)
+    {
+
+    }
+
+    private void collectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void btnAdd_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnDeleteAll_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
 
